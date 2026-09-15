bfdb645b315627cb46ace363dc0872dfc3424b3b
Kusumakar Rokkam <krokkam@student.gitam.edu>
2026-09-15 13:40:35 +0530

    Treat legacy browser scripts as modules

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -13,5 +13,4 @@ declare module "react/jsx-runtime" {
   export function jsx(type: unknown, props: unknown, key?: unknown): unknown;
   export function jsxs(type: unknown, props: unknown, key?: unknown): unknown;
 }
-declare module "*enhancements" {}
 declare namespace JSX { interface IntrinsicElements { [key: string]: any } }
